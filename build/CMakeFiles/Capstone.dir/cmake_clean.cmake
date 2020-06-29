e62f28e77ae308d13bd5ca15bd7cc93d0c601fa4
Kyle Coelho <coelho.k@husky.neu.edu>
2020-06-28 22:18:16 -0400

    Remove build

---
+++ build/CMakeFiles/Capstone.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Capstone.dir/src/main.cpp.o"
+  "CMakeFiles/Capstone.dir/src/body.cpp.o"
+  "CMakeFiles/Capstone.dir/src/bounds.cpp.o"
+  "Capstone.pdb"
+  "Capstone"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Capstone.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
